f9f7f65ddae2bf6b0354bed4fea52e7c3ceb340d
Matt Savoia <matt@1-800-rad-dude.com>
2022-06-05 22:06:45 -0400

    Update GuessInputChanged, make case-insensitive

--- src/ReverseWordle.elm
+++ src/ReverseWordle.elm
@@ -253,7 +253,7 @@ update msg model =
                     { model | guessInput = RejectedInput (guessInputToString model.guessInput) guessFeedback }
 
         GuessInputChanged guessText ->
-            { model | guessInput = GuessInput guessText }
+            { model | guessInput = GuessInput (guessText |> String.toLower) }
 
         ClickedGuess i ->
             { model | currentGuess = i }
